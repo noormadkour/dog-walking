eaddda323b9c4acc342ac4bd4958ff652c681ff9
Noor Madkour <noormadkour@gmail.com>
2023-08-01 23:52:53 -0500

    added final tweaks to the event listeners so that the window alerts displayed the correct information since changing the database to add a city property

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,12 +1,17 @@
-import { getWalkers } from "./database.js";
+import { getCities, getWalkers } from "./database.js";
 
 const walkers = getWalkers();
+const cities = getCities();
 
 document.addEventListener("click", (theClickEvent) => {
   const whatWasClickedOn = theClickEvent.target;
 
   if (whatWasClickedOn.dataset.type === "walker") {
-    window.alert(`This walker works in ${whatWasClickedOn.dataset.city}`);
+    for (const city of cities) {
+      if (city.id === parseInt(whatWasClickedOn.dataset.cityid)) {
+        window.alert(`This walker works in ${city.name}`);
+      }
+    }
   }
 });
 
@@ -15,7 +20,7 @@ export const Walkers = () => {
 
   for (const walker of walkers) {
     walkerHTML += `<li  data-id="${walker.id}"
-                        data-city="${walker.city}"
+                        data-cityid="${walker.city_id}"
                         data-type="walker"
     >${walker.name}</li>`;
   }
